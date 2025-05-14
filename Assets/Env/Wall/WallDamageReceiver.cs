0cd04298805ed8186f516ae77afd9dbeeba80c3b
VanTuyen12 <tvtuyen121@gmail.com>
2025-05-15 02:44:00 +0700

    t

--- Assets/Env/Wall/WallDamageReceiver.cs
+++ Assets/Env/Wall/WallDamageReceiver.cs
@@ -13,7 +13,7 @@ public class WallDamageReceiver : DamageRecever
     {
         if(this.boxCollider != null) return;
         this.boxCollider = this.GetComponent<BoxCollider>();
-        this.boxCollider.isTrigger = true;
+        this.boxCollider.isTrigger = false;
         
         Debug.Log(transform.name + " LoadBoxCollider", gameObject);
     }
