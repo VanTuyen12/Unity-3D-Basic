e133ba01d17d1d53f64197c878e99d34e1ef5339
VanTuyen12 <tvtuyen121@gmail.com>
2025-05-16 12:13:08 +0700

    s

---
+++ Assets/Player/CrosshairPointer/CrosshairPointer.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+public class CrosshairPointer : MonoBehaviour
+{
+    //Code Hong Tam
+    protected float maxDistance = 100f;
+    [SerializeField] LayerMask layerMask= -1;
+
+    protected virtual void Update()
+    {
+        this.Pointing();
+    }
+
+    protected virtual void Pointing()
+    {
+        //Screen.width: Lấy chiều rộng của màn hình
+        //Screen.height: Lấy chiều cao của màn hình
+        //Chia cho 2 để tìm ra điểm chính giữa.
+        //Tọa độ Z là 0, vì tọa độ trên màn hình thường chỉ có X và Y thôi.
+        Vector3 screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
+        //Camera.main: Lấy cái camera chính trong scene của bạn (cái camera được tag là "MainCamera").
+        //ScreenPointToRay: Nó lấy một điểm trên màn hình 2D chuyển nó thành một "tia sáng"
+        //ray : chứa thông tin về điểm bắt đầu và hướng đi của tia sáng.
+        Ray ray = Camera.main.ScreenPointToRay(screenCenter);//tia sáng này bắt đầu từ vị trí của camera và đi xuyên qua cái điểm giữa màn hình đó, hướng vào thế giới 3D.
+
+        #region Giai Thich
+        //out RaycastHit hit: Nếu tia sáng đâm vào vật gì đó, thông tin về điểm va chạm sẽ được lưu vào biến hit.
+        //maxDistance: giới hạn khoảng cách tối đa mà tia sáng này được đi.
+        //chỉ định những "lớp" (layer) nào mà tia sáng này được phép đâm vào
+        #endregion
+        
+        if (Physics.Raycast(ray, out RaycastHit hit, maxDistance, layerMask)) //return True or False
+        {
+            //hit.point: vị trí nơi tia sáng đâm vào bề mặt của vật thể đầu tiên nó gặp.
+            transform.position = hit.point;//di chuyen den vi tri va cham do
+            
+        }
+      
+    }
+}
---
+++ Assets/Scripts/HideMouse.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class HideMouse : MonoBehaviour
+{
+    //Su ly chuot quay man hinh
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
